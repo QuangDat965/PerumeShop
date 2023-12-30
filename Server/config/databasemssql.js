4bc623cba98b9bb337c7aa2843c5391e5f6461eb
Quang Dat <thanhdat8791@gmail.com>
2023-12-30 23:06:45 +0700

    add env

--- Server/config/databasemssql.js
+++ Server/config/databasemssql.js
@@ -1,8 +1,15 @@
 // db.js
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
-const sequelize = new Sequelize('perfum', 'sa', '16122001', {
-  host: 'localhost',
+const dbname = process.env.DN_NAME;
+const dbhost = process.env.DB_HOST;
+const dbuser = process.env.DB_USER;
+const dbpassword = process.env.DB_PASSWOED;
+
+
+const sequelize = new Sequelize(dbname, dbuser, dbpassword, {
+  host: dbhost,
   dialect: 'mssql',
   dialectOptions: {
     options: {
